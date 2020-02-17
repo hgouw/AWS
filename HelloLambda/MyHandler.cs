49422f3b5388444696e0ffaeeb1f0c4150f59a1c
Herman Gouw <hermangouw@gmail.com>
2020-02-17 15:18:05 +1100

    Added more info

--- HelloLambda/MyHandler.cs
+++ HelloLambda/MyHandler.cs
@@ -1,8 +1,27 @@
 ﻿using System;
+using Amazon.Lambda.Core;
+using Amazon.Lambda.Serialization.Json;
+using System.Threading.Tasks;
 
 namespace HelloLambda
 {
     public class MyHandler
     {
+        [LambdaSerializer(typeof(JsonSerializer))]
+        public Result HandleFunction(Request request)
+        {
+            return new Result
+            {
+                HelloWorld = request.Name
+            };
+        }
+    }
+    public class Request
+    {
+        public string Name { get; set; }
+    }
+    public class Result
+    {
+        public string HelloWorld { get; set; }
     }
 }
